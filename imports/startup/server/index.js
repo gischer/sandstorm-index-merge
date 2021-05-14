90b0e353058e0a90fe73630f97b0409eb6bfe348
Jay Gischer <jay@gischer.net>
2021-05-14 13:36:20 -0700

    Just adding some basics.

---
+++ imports/startup/server/index.js
@@ -0,0 +1,21 @@
+// Import server startup through a single index entry point
+
+import './register-api.js';
+import { exec } from 'child_process';
+
+if (Meteor.isServer) {
+  /*
+  exec(`prlimit --nofile`, (error, stderr, stdout) => {
+    console.log(`prlimit(0): ${stderr}`)
+    exec('prlimit --nofile=8192', (error, stderr, stdout) => {
+      if (error) {
+        console.log(`prlimit Error: ${error}`);
+      }
+      if (stderr) {
+        console.log(`prlimit stderr: ${error}`);
+      }
+      console.log(`prlimit: ${stdout}`)
+    })
+  })
+  */
+}
