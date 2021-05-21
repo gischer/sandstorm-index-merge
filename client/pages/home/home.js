18bf8afdfe0a84b7ef4b4b0d9c2719d36f6de38a
Jay Gischer <jay@gischer.net>
2021-05-21 13:45:59 -0700

    Put publicIdPath on Index page

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -1,7 +1,16 @@
 import { Template } from 'meteor/templating';
 
+import { SandstormInfo } from '/imports/api/sandstorm';
 import './home.html';
 
 Template.Home.onCreated(function() {
   Meteor.call('sandstorm.initialize');
+  this.subscribe('sandstorm.info');
+})
+
+Template.Home.helpers({
+  indexUrl() {
+    const info = SandstormInfo.findOne();
+    return (typeof info == 'undefined') ? "???" : info.publicIdPath;
+  }
 })
